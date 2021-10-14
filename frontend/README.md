5eeeb09897c48a8b7319251be380ecfa2c77acf3
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2021-10-14 19:13:09 +0300

    fixed the problem with docker

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# BookForum
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.5.
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,29 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksComponent } from './components/books/books.component';
-import { BooksDetailsComponent } from './components/books-details/books-details.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { ClarityModule, ClrIconModule } from '@clr/angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BooksComponent } from './components/books/books.component';
+import { BooksDetailsComponent } from './components/books-details/books-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent,
-    BooksDetailsComponent,
     FooterComponent,
     HeaderComponent,
-    SidebarComponent
+    BooksComponent,
+    BooksDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ClarityModule,
-    BrowserAnimationsModule,
-    ClrIconModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
