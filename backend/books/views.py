d12683b9cb0c2bf0a4a61e719aad3551daab973f
FarinHeiT <yariksuharik85@gmail.com>
2021-10-14 18:22:20 +0300

    list/retrieve/update/delete actions for Book model

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,8 +1,44 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views import View
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins, generics
+from rest_framework import permissions
+
+from books.models import Book
+from books.permissions import IsCreatorOrReadOnly
+from books.serializers import BookSerializer
 
 
-class HelloView(View):
-    def get(self, request):
-        return HttpResponse(f"<h1>Hello {request.META['REMOTE_ADDR']}</h1>")
+class BookList(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['published_year', 'author', 'genres']
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class BookDetails(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsCreatorOrReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
