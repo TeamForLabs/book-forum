8e9cd40515d6329888bc8f5e47965eb03bd60ae6
FarinHeiT <yariksuharik85@gmail.com>
2021-10-22 12:05:20 +0300

    filter books by title and description

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, filters
 from rest_framework import permissions
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -16,8 +16,10 @@ class BookList(mixins.ListModelMixin,
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     filterset_fields = ['published_year', 'author', 'genres']
+
+    search_fields = ['title', 'description']
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
